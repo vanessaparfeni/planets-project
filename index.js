6906cc9474ee76b1eac93a9534306d91c060d211
Vanessa <vanessaparfeni123@mail.ru>
2024-02-12 18:29:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
+
+const result = [];
+
+fs.createReadStream('kepler_data.csv')
+    .on('data', (data) => {
+        result.push(data)
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log(result);
+        console.log("Готово!")
+    });
